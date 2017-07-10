75e8014640f22998d4e6b93f77297d20950e6b07
Kristin Glaze <ikglaze33@gmail.com>
2017-07-10 02:01:59 -0400

    Hangman game includes messages for when game is lost or won, or if an incorrect letter was guessed a second time. Prevents user from continuing to enter guesses if they already lost.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,30 +4,74 @@ arrayDisplayedGuess.fill("_");
 var arrayWrongGuessedLetters = [];
 var numGuesses = 0;
 var incorrectGuesses = 0;
+var attemptsRemaining = 12;
+var canPlay = true;
 document.addEventListener("DOMContentLoaded", function(event) { 
 	var guessElement = document.querySelector(".my-guess");
+	var attemptsRemainingElement = document.querySelector(".attempts-remaining");
+	var wrongGuessesElement = document.querySelector(".wrong-guesses");
+	var gameStatusMessageElement = document.querySelector(".game-status-message");
+	var repeatedIncorrectGuessElement = document.querySelector(".repeated-incorrect-guess");
   	updateDisplayedGuesses();
+  	updateAttemptsRemaining();
 	document.addEventListener('keydown', function(e) {
 		debugger;
 		var alnumRegex = /^[0-9a-z]+$/;
-	    if(e.key.match(alnumRegex)) {
+	    if(e.key.match(alnumRegex) && canPlay) {
 	    	var guessedLetter = e.key;
 	    	if(currentWord.includes(guessedLetter)) {
+	    		hideElement(repeatedIncorrectGuessElement);
+	    		var isBlanksLeft = false;
 	    		for (var i = 0; i < currentWord.length; i++) {
 	    			if(currentWord[i] === guessedLetter) {
 	    				arrayDisplayedGuess[i] = guessedLetter;
 	    				updateDisplayedGuesses();
 	    			}
+	    			if(isBlanksLeft === false && arrayDisplayedGuess[i] === "_") {
+	    				isBlanksLeft = true;
+	    			}
 	    		}
-
+	    		if(isBlanksLeft === false && attemptsRemaining > 0) {
+	    			doWonGameActions();
+	    		}
 	    	} else {
-				arrayWrongGuessedLetters.push(guessedLetter);
-				incorrectGuesses++;
+	    		if(!arrayWrongGuessedLetters.includes(guessedLetter)) {
+	    			hideElement(repeatedIncorrectGuessElement);
+					incorrectGuesses++;
+					attemptsRemaining--;
+					updateAttemptsRemaining();
+					arrayWrongGuessedLetters.push(guessedLetter);
+					updateFailedGuesses();
+					if(attemptsRemaining <= 0) {
+						doLostGameActions();
+					}
+	    		} else {
+	    			showElement(repeatedIncorrectGuessElement);
+	    		}
 	    	}
 	    	numGuesses++;
 	    }
 	}); 
 	function updateDisplayedGuesses() {
 		guessElement.innerText = arrayDisplayedGuess.join("");
+	}
+	function updateAttemptsRemaining() {
+		attemptsRemainingElement.innerText = attemptsRemaining;
+	}
+	function updateFailedGuesses() {
+		wrongGuessesElement.innerText = arrayWrongGuessedLetters.join(", ");
+	}
+	function doLostGameActions() {
+		gameStatusMessageElement.innerText = "Sorry, you lost.";
+		canPlay = false;
+	}
+	function doWonGameActions() {
+		gameStatusMessageElement.innerText = "Congratulations!!! You won.";
+	}
+	function hideElement(element) {
+		element.style.display = "none";
+	}
+	function showElement(element) {
+		element.style.display = "block";
 	}
 });
